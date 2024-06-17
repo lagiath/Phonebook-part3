68b29abc27bba6215e0f440c4db63e879f058ad7
Notfatdan <84719438+lagiath@users.noreply.github.com>
2024-06-17 16:00:18 +1000

    uibuild

--- modules/person.js
+++ modules/person.js
@@ -17,13 +17,25 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
+const validator = (val) => {
+
+}
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
     required: true
   }, 
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: v => {
+      return /^\d{2,3}-\d+$/.test(v)
+      }
+    },
+    required: true
+  } 
 })
 
 const Person = mongoose.model('Person', personSchema)
